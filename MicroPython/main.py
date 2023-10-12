2cc775952ca1c1f91efcb5dc53c77048c859c813
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-10-12 22:13:33 +0000

    washing machines

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,10 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Michael Bruneau
+Created on: Oct 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
-
-display.scroll("Hello, World!")
+while True
+    
