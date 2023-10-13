5d36d91dd62832e37cdcee8e6848f36fde55a288
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-10-13 13:00:20 -0400

    starting code

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Michael B
+ * Created on: Oct 2023
+ * This program diplays traffic lights on neopixels
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+let neopixelStrip: neopixel.Strip = null
+
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
