277dd9d0acfb09a5e1eeb5166dc3fbbf30a47075
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-10-13 13:25:03 -0400

    Typescriptversion

--- main.ts
+++ main.ts
@@ -18,3 +18,11 @@ neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.show()
+
+input.onButtonPressed(Button.A, function () {
+  neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+  basic.pause(1000)
+  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+  basic.pause(1000)
+  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+})
