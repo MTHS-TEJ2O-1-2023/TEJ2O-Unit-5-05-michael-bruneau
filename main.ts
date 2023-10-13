155bfdff2340318b0d813bd3302078455828ca53
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-10-13 17:22:49 -0400

    Typescript version fixed

--- main.ts
+++ main.ts
@@ -5,29 +5,52 @@
  * This program diplays traffic lights on neopixels
 */
 
-basic.clearScreen()
-basic.showIcon(IconNames.Happy)
-
+// variables
 let neopixelStrip: neopixel.Strip = null
 
+// setup
+basic.clearScreen()
 neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-
-// zero all the colors
 neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
   basic.clearScreen()
+
+  // turn neopixelStrip 2 Green
   neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
   neopixelStrip.show()
   basic.pause(1000)
+
+  // Turn off neopixelStrip 2
+  neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.show()
+  basic.pause(1000)
+
+  // turn neopixelStrip 1 Yellow
   neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
   neopixelStrip.show()
   basic.pause(1000)
+
+  // turn off neopixelStrip 1
+  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.show()
+  basic.pause(1000)
+
+  // turn neopixelStrip 0 Red
   neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
   neopixelStrip.show()
-  pause(1000)
+  basic.pause(1000)
+
+  // turn off neopixelStrip 0
+  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.show()
+  basic.pause(1000)
+
+  // reset screen
+  basic.showIcon(IconNames.Happy)
 })
