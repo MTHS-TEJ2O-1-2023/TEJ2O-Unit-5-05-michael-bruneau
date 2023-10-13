c0ecb279364aadb48658bae10e004dd54509a4af
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-10-13 22:15:44 +0000

    yayyyyy

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -7,6 +7,7 @@ This module is a Micro:bit MicroPython program
 from microbit import *
 
 neopixel_strip = neopixel.NeoPixel(pin16, bpp=3)
-
+neopixel_strip[0]= (0, 0, 0)
+neopixel_strip.show()
 display.clear()
 display.show(Image.HAPPY)
