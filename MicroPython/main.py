d061d9643e14f44472a3d9a7341f4096ccbb2dca
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-10-14 16:09:21 +0000

    yes

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -2,12 +2,28 @@
 Created by: Michael Bruneau
 Created on: Oct 2023
 This module is a Micro:bit MicroPython program
+neopixel_random.py
 """
 
 from microbit import *
+import neopixel
 
-neopixel_strip = neopixel.NeoPixel(pin16, bpp=3)
-neopixel_strip[0] = (0, 0, 0)
-neopixel_strip.show()
+np = neopixel.NeoPixel(pin16, 3)
+
+for pixel_id in range(0, len(np)):
+    red = range(0, 255)
+    yellow = range(0, 255)
+    Green = range(0, 255)
+np[pixel_id] = (red, Green, yellow,)
+
 display.clear()
 display.show(Image.HAPPY)
+print(np(0))
+np[0] = (255, 0, 0)
+print(np(1))
+np[1] = (0, 0, 0)
+print(np(2))
+np[2] = (0, 0, 0)
+print(np(3))
+np[3] = (0, 0, 0)
+np.show()
