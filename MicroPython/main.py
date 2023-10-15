01723ba9222dbf35998b66100c56a489bc27fd40
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-10-15 20:33:01 +0000

    python version 4,4

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -7,19 +7,9 @@ neopixel_random.py
 
 from microbit import *
 import neopixel
-from random import randint
+
 
 np = neopixel.NeoPixel(pin16, 5)
-
-for pixel_id in range(0, len(np)):
-    red = randint(0, 255)
-    yellow = randint(0, 255)
-    green = randint(0, 255)
-    np[pixel_id] = (
-        red,
-        green,
-        yellow,
-    )
 
 display.clear()
 display.show(Image.HAPPY)
