bb4372e73f1c402cf61b3db46f2036cf8fa3d865
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-10-17 17:07:50 +0000

    annotatins

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -8,7 +8,7 @@ neopixel_random.py
 from microbit import *
 import neopixel
 
-
+# setup
 np = neopixel.NeoPixel(pin16, 5)
 
 display.clear()
@@ -23,20 +23,30 @@ print(np[3])
 np[3] = (0, 0, 0)
 np.show()
 
+# loop
 while True:
+    # Button A Pressed
     if button_a.is_pressed():
         display.clear()
+
+        # neopixel 2 turn green
         np[2] = (0, 255, 0)
         np.show()
         sleep(1000)
+
+        # turn off neopixel 2 and turn on neopixel 1 to yellow
         np[2] = (0, 0, 0)
         np[1] = (255, 255, 0)
         np.show()
         sleep(1000)
+
+        # Turn off neopixel 1 and turn on neopixel 0 to red
         np[1] = (0, 0, 0)
         np[0] = (255, 0, 0)
         np.show()
         sleep(1000)
+
+        # turn off neopixel 0
         np[0] = (0, 0, 0)
         np.show()
         display.show(Image.HAPPY)
